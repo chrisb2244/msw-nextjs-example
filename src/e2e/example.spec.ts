1b8c729adc5a4916f3c77d8c7570a0e0ff6c7d4d
Christian Butcher <chrisb2244@gmail.com>
2024-02-15 04:10:35 +0900

    Move remote into beforeAll/afterAll calls.

--- src/e2e/example.spec.ts
+++ src/e2e/example.spec.ts
@@ -2,27 +2,36 @@ import { test, expect } from "@playwright/test";
 import { HttpResponse, http } from "msw";
 import { setupRemoteServer } from "msw/node";
 
+const remote = setupRemoteServer();
+
+test.beforeAll(async () => {
+  await remote.listen({ port: 3030 });
+});
+test.afterEach(() => {
+  remote.resetHandlers();
+});
+test.afterAll(async () => {
+  await remote.close();
+});
+
 test("has title", async ({ page }) => {
-  await page.goto("/");
+  await page.goto("/", { timeout: 10000 });
 
   // Expect a title "to contain" a substring.
   const elemTitle = page.getByText(/sunt aut facere/);
   await expect(elemTitle).toBeVisible();
 });
 
-test.only("can be mocked", async ({ page }) => {
-  const remote = setupRemoteServer(
-    ...[
-      http.get("https://jsonplaceholder.typicode.com/posts/1", () => {
-        return HttpResponse.json({ title: "Hello", body: "World" });
-      }),
-    ]
+test("can be mocked", async ({ context }) => {
+  remote.use(
+    http.get("https://jsonplaceholder.typicode.com/posts/1", () => {
+      return HttpResponse.json({ title: "Hello", body: "World" });
+    })
   );
-  await remote.listen({ port: 3030 });
 
-  await page.goto("/", { timeout: 20000 });
+  const page = await context.newPage();
+  await page.goto("/", { timeout: 10000 });
+
   const elemTitle = page.getByText("Hello");
   await expect(elemTitle).toBeVisible();
-
-  await remote.close();
 });
